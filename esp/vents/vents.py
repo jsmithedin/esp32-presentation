32d9be916f899c2a01ee2b7892888889e2df9066
Jamie Smith <jamie@jsmth.co.uk>
2018-06-04 15:49:45 +0100

    WIP for ESP code

---
+++ esp/vents/vents.py
@@ -0,0 +1,41 @@
+import time
+import json
+
+from machine import Pin
+from AWSIoTPythonSDK.MQRRLib import AWSIoTMQTTClient
+
+
+# GPIO16 (D0) is the internal LED for NodeMCU
+led = Pin(16, Pin.OUT)
+
+led.high()
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+
+time.sleep(2)
+
+while True:
+    time.sleep(1)
